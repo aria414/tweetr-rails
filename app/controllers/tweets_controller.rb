7fa77eef66de2d3c6af571f5b7ec069878c8a276
aria414 <ayng13@yahoo.com>
2021-02-23 19:48:27 -0500

    Commit 8: Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,20 @@ class TweetsController < ApplicationController
         tweetFound = Tweet.find(params[:id])
         render(json: {tweet: tweetFound})
     end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render(status: 200, json: {tweetMade: tweet} )
+        else
+            render(status: 422, json: {tweetMade: tweet, errors: tweet.errors} )
+        end
+    end
+    
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
+    end
 end
